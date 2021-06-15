d72d68a04a4654babd1e93b9f2e94b3d2a712e96
Paul Carvalho <pcarvalh@ucsc.edu>
2021-06-15 11:18:26 -0700

    added N0 as an input for the function, removed old code for non-parallel for loop

--- run_model.R
+++ run_model.R
@@ -1,4 +1,4 @@
-run_model <- function(effort, gear.mgmt, nsc, nspecies, t, Lmat, M1, phi, L.lower, L.upper, W.a, W.b, q, alpha, beta, suit, ration, other, weight, sc_Linf, phi.min){
+run_model <- function(effort, gear.mgmt, nsc, nspecies, t, Lmat, M1, phi, L.lower, L.upper, W.a, W.b, q, alpha, beta, suit, ration, other, weight, sc_Linf, phi.min, N0){
      #' run_model
      #' 
      #' @description Run fisheries model with a single management scenario and set of parameters.
@@ -24,6 +24,7 @@ run_model <- function(effort, gear.mgmt, nsc, nspecies, t, Lmat, M1, phi, L.lowe
      #' @param weight Weight of each functional group and size class
      #' @param sc_Linf Size class at asymptotic length
      #' @param phi.min Minimum timestep
+     #' @param N0 intial population size
      #'
      #' @return List with (1) abundance, (2) biomass, (3) catch in numbers, and (4) catch in biomass for each size class, functional 
      #' group, timestep, and effort level; and (5) spawning stock biomass and (6) recruitment for each functional group, timestep and
@@ -43,6 +44,7 @@ run_model <- function(effort, gear.mgmt, nsc, nspecies, t, Lmat, M1, phi, L.lowe
      registerDoParallel(cluster)
 
      results <- foreach(e = 1:length(effort), .combine = 'rbind', .multicombine = TRUE) %dopar% {
+             source('model_functions.R')        
              # determine fishing mortality given fishing effort
              fishing.effort <- (effort[e] / ngears) # divide effort among gears
              fishing.effort <- fishing.effort * gear.mgmt
@@ -83,7 +85,7 @@ run_model <- function(effort, gear.mgmt, nsc, nspecies, t, Lmat, M1, phi, L.lowe
      }
      parallel::stopCluster(cluster)
      
-     for(i in length(effort)){
+     for(i in 1:length(effort)){
              N.ijte[, , , i]  <- as.array(results[i, 1])[[1]]
              B.ijte[, , , i]  <- as.array(results[i, 2])[[1]]
              cN.ijte[, , , i] <- as.array(results[i, 3])[[1]]
@@ -92,62 +94,6 @@ run_model <- function(effort, gear.mgmt, nsc, nspecies, t, Lmat, M1, phi, L.lowe
              R.ijte[, , , i]  <- as.array(results[i, 6])[[1]]
      }
      
-     # tmp <- results[,1]
-     # N.ijte 
-     # 
-     # x.test2 <- array(NA, dim = c(2,2,3,5))
-     # for(i in 1:5){
-     #         x.test2tmp <- array(as.array(x.test)[i, ], dim = c(2, 2, 3))
-     #         x.test2[, , , i] <- x.test2tmp 
-     # }
-     
-#      
-#      for(e in 1:length(effort)){
-#         # determine fishing mortality given fishing effort
-#         fishing.effort <- (effort[e] / ngears) # divide effort among gears
-#         fishing.effort <- fishing.effort * gear.mgmt
-#         F.mort         <- F_mortality(nsc, nspecies, fishing.effort, gear.mgmt, q)
-#         # set up population
-#         N.ijt      <- array(NA, dim = c(nsc, nspecies, t))
-#         N.ijt[,,1] <- N0
-#         S.ijt      <- array(NA, dim = c(1, nspecies, t))
-#         R.ijt      <- array(NA, dim = c(1, nspecies, t))
-#         c.ijt      <- array(NA, dim = c(nsc, nspecies, t))
-#         c.ijt[,,1] <- 0
-#         # run model
-#         for(y in 2:t){
-#                 # recruitment
-#                 # calculate ssb and recruitment with Beverton-Holt
-#                 S.ijt[,,y] <- calc_ssb(nspecies, nsc, L.lower, L.upper, N.ijt, y, Lmat)
-#                 R.ijt[,,y] <- (S.ijt[,,y]) / (alpha + (beta*S.ijt[,,y]))
-#                 N.tmp      <- N.ijt[,,y-1]
-#                 N.tmp[1,]  <- N.tmp[1,] + R.ijt[,,y]
-#                 # calculate predation mortality
-#                 M2 <- calc_M2(N=N.tmp, suit, ration, nspecies, nsc, other, weight, sc_Linf, phi.min)
-#                 # calculate catch
-#                 c.tmp <- (F.mort / (F.mort + M1 + M2)) * ((1 - (exp(-(F.mort + M1 + M2)))) * N.tmp)
-#                 # impose mortality
-#                 N.tmp <- N.tmp * exp(-(F.mort + M1 + M2))
-#                 # allow growth
-#                 N.tmp <- calc_growth(N.tmp, phi, nsc, nspecies)
-#                 # save abundance and catch
-#                 N.ijt[,,y] <- N.tmp
-#                 c.ijt[,,y] <- c.tmp
-#         }
-#         # calculate biomass and catch(biomass)
-#         B.ijt  <- calc_bio(N.ijt, t, L.lower, L.upper, W.a, W.b, nsc, nspecies)
-#         cB.ijt <- calc_bio(c.ijt, t, L.lower, L.upper, W.a, W.b, nsc, nspecies)
-#         S.ijt  <- calc_bio(S.ijt, t, L.lower, L.upper, W.a, W.b, 1, nspecies)
-#         # save data
-#         N.ijte[,,,e]  <- N.ijt
-#         B.ijte[,,,e]  <- B.ijt
-#         cN.ijte[,,,e] <- c.ijt
-#         cB.ijte[,,,e] <- cB.ijt
-#         S.ijte[,,,e]  <- S.ijt
-#         R.ijte[,,,e]  <- R.ijt
-#      }
-#      proc.time() - ptm
-#      
      out <- list(N.ijte, B.ijte, cN.ijte, cB.ijte, S.ijte, R.ijte)
      return(out)
 }
